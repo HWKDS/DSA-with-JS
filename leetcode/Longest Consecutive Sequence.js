56606c580cfee65e94f283f5d241d1101cabd1bb
HWKDS <yomikka5555@gmail.com>
2026-05-04 01:34:55 +0530

    new question

---
+++ leetcode/Longest Consecutive Sequence.js
@@ -0,0 +1,16 @@
+var longestConsecutive = function (nums) {
+  let set = new Set(nums);
+  let longest = 0;
+  for (let num of set) {
+    if (!set.has(num - 1)) {
+      let current = num;
+      let count = 1;
+      while (set.has(curent + 1)) {
+        current++;
+        count++;
+      }
+      longest = Math.max(longest, count);
+    }
+  }
+  return longest;
+};
