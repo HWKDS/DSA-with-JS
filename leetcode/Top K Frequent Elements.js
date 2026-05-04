e75ec62dd012583a13b3a9410e2cf0ee251cf977
HWKDS <yomikka5555@gmail.com>
2026-05-04 13:44:42 +0530

    new method

--- leetcode/Top K Frequent Elements.js
+++ leetcode/Top K Frequent Elements.js
@@ -1,3 +1,5 @@
+// time: O(nlogn)
+//space: O(n)
 var topKFrequent = function (nums, k) {
   let map = new Map();
   for (let num of nums) {
@@ -5,4 +7,27 @@ var topKFrequent = function (nums, k) {
   }
   let arr = Array.from(map.entries()).sort((a, b) => b[1] - a[1]);
   return arr.slice(0, k).map((item) => item[0]);
+};
+
+// bucket method
+var topKFrequent = function (nums, k) {
+  let map = new Map();
+  for (let val of nums) {
+    map.set(val, (map.get(val) || 0) + 1);
+  }
+  let bucket = Array(nums.length + 1)
+    .fill(0)
+    .map(() => []);
+  for (let [num, freq] of map) {
+    bucket[freq].push(num);
+  }
+  let result = [];
+  for (let i = bucket.length - 1; i >= 0; i--) {
+    for (let num of bucket[i]) {
+      result.push(num);
+    }
+    if (result.length === k) {
+      return result;
+    }
+  }
 };
