ddbcb3d69db6e4ae373e0723243cea1a00980c85
HWKDS <yomikka5555@gmail.com>
2026-05-05 23:26:02 +0530

    new Question

---
+++ leetcode/First Unique Character in a String.js
@@ -0,0 +1,12 @@
+var firstUniqChar = function (s) {
+  let map = new Map();
+  for (let val of s) {
+    map.set(val, (map.get(val) || 0) + 1);
+  }
+  for (let i = 0; i < s.length; i++) {
+    if (map.get(s[i]) === 1) {
+      return i;
+    }
+  }
+  return -1;
+};
