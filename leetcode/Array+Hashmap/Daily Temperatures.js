0447cf7acba8cfa7ce42da899c9d8d01c1394b87
HWKDS <yomikka5555@gmail.com>
2026-05-13 23:11:10 +0530

    Daily Temperatures.js

---
+++ leetcode/Array+Hashmap/Daily Temperatures.js
@@ -0,0 +1,15 @@
+var dailyTemperatures = function (temperatures) {
+  let stack = [];
+  let ans = new Array(temperatures.length).fill(0);
+  for (let i = 0; i < temperatures.length; i++) {
+    while (
+      stack.length &&
+      temperatures[i] > temperatures[stack[stack.length - 1]]
+    ) {
+      let prevIndex = stack.pop();
+      ans[prevIndex] = i - prevIndex;
+    }
+    stack.push(i);
+  }
+  return ans;
+};
