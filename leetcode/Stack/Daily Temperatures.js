c94de8248652f07c4f50f00c119f6228cd0c45a9
HWKDS <yomikka5555@gmail.com>
2026-05-13 23:15:59 +0530

    Daily Temperatures.js: test cases

--- leetcode/Stack/Daily Temperatures.js
+++ leetcode/Stack/Daily Temperatures.js
@@ -13,3 +13,6 @@ var dailyTemperatures = function (temperatures) {
   }
   return ans;
 };
+console.log(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]));
+console.log(dailyTemperatures([30, 40, 50, 60]));
+console.log(dailyTemperatures([30, 60, 90]));
